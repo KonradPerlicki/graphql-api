0db9da8278b047f3b3273f39b19f286702ec283d
Konrad Perlicki <Konrad.perlicki01@gmail.com>
2023-04-05 18:32:52 +0200

    [Fix] - env path

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-dotenv.config({ path: "./.env" });
+dotenv.config();
 import AppDataSource from "./data-source";
 import sessionConfig from "./session";
 import session from "express-session";
