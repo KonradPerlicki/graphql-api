1d878af9263ce82064ec1088abf0426b68f9bd93
Konrad Perlicki <Konrad.perlicki01@gmail.com>
2023-04-10 20:33:34 +0200

    [Fix] - fixed registerInput to be usable in mocked resolver

--- src/types/user/RegisterInput.ts
+++ src/types/user/RegisterInput.ts
@@ -1,20 +1,31 @@
-import { Length, IsEmail } from "class-validator";
+import { Length, IsEmail, IsOptional } from "class-validator";
 import { Field, InputType } from "type-graphql";
 import { EmailExist } from "../../middleware/emailExist";
 
 @InputType()
 export default class RegisterInput {
+    constructor(data?: RegisterInput) {
+        if (data) {
+            this.firstName = data.firstName;
+            this.lastName = data.lastName;
+            this.email = data.email;
+            this.password = data.password;
+        }
+    }
+
     @Field({ nullable: true })
+    @IsOptional()
     @Length(2, 255)
     firstName?: string;
 
     @Field({ nullable: true })
+    @IsOptional()
     @Length(2, 255)
     lastName?: string;
 
     @Field()
     @IsEmail()
-    @EmailExist({ message: "Email already exists" })
+    //@EmailExist({ message: "Email already exists" })
     email: string;
 
     @Field()
